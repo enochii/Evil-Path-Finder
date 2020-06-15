325fde7a4b7d6e5f0066956c81156652e072326b
hangshuaibi <1440620378@qq.com>
2020-06-15 12:03:48 +0800

    algo selector

--- src/components/Header/AlgoSelector.jsx
+++ src/components/Header/AlgoSelector.jsx
@@ -4,6 +4,8 @@
 
 import React, {useState, useContext} from "react";
 import { Context} from '../../Provider.jsx';
+// import { DropdownButton, Dropdown } from 'react-bootstrap'
+import ItemRenderer from './DropDown'
 
 
 const AlgoSelector = (props) => {
@@ -15,15 +17,20 @@ const AlgoSelector = (props) => {
     // 倒序... 这样用户的算法就会排在最前面--
     const listItems = algorithms.current.reverse().map((algo_name) =><option key={algo_name}> {algo_name}</option>);
 
+    const items = ['122'];
+
     return (
-        <select
-        className="content-header__select"
-        onChange={onAlgoChange}
-        id="algorithm"
-        disabled={isVisualized}
-        >
-        {listItems}
-        </select>
+        // <select
+        // className="content-header__select"
+        // onChange={onAlgoChange}
+        // id="algorithm"
+        // disabled={isVisualized}
+        // >
+        // {listItems}
+        // </select>
+        // <div >
+        <ItemRenderer className="content-header__select" options={items} title={'nb'}></ItemRenderer>
+        // </div>
     );
 }
 
